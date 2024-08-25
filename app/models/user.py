d4fe0db3fe0b19ccb40096f3d494d73ae4e923a9
alexey <eskealexey@mail.ru>
2024-08-25 14:23:21 +0300

    Домашнее задание по теме "Модели SQLALchemy. Отношения между таблицами."

---
+++ app/models/user.py
@@ -0,0 +1,23 @@
+from app.backend.db import Base
+from sqlalchemy import Column, Integer, String, ForeignKey, Boolean, Index
+from sqlalchemy.orm import relationship
+
+from app.models import *
+
+
+class User(Base):
+    __tablename__ = "users"
+    __table_args__ = {"keep_existing": True}
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String)
+    firstname = Column(String)
+    lastname = Column(String)
+    age = Column(Integer)
+    slug = Column(Integer, unique=True, index=True)
+    tasks = relationship("Task", back_populates="user")
+
+
+#
+# from sqlalchemy.schema import CreateTable
+#
+# print(CreateTable(User.__table__))
